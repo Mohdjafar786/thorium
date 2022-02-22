7f84cc5807e015782cdad311fbcc1357d021c912
Mohdjafar786 <mj029044@gmail.com>
2022-02-22 20:54:34 +0530

    change in route.js file

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,13 +1,52 @@
+
 const express = require('express');
+
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
+
+
+router.get('/movies', function (req, res) {
+   res.send('["Fukrey","Delhi","Dabang","Rockstar","suryawansh"]')
+});
+
+
+router.get('/movies/:movieId', function(req,res){
+mov =["Fukrey","Delhi","Dabang","Rockstar","suryawansh"]
+ let value = req.params.movieId;
+ if(value>mov.length-1)
+ {
+     res.send(mov[value])
+ }
+ else
+ { res.send(mov[value])
+
+ }
+})
+// 3. this API will fetch all movies from array
+router.get('/moviez',function(req,res){
+   res.send([ {id: 1,name:'The Shining' }, {id: 2, name:'Incendies' }, {id: 3,name: 'Rang de Basanti'}, {id: 4, name: 'welcome' },{id: 5, name: 'Finding Demo' }])
+})
+
+router.get('/films/:filmId', function(req,res){
+   let movie=[ {id: 1,name:'The Shining' }, {id: 2, name:'Incendies' },{id: 3,name: 'Rang de Basanti'}, {id: 4, name: 'welcome' },{id: 5, name: 'Finding Demo' }]
+    let value =req.params.filmId;
+    let found=false;
+    for(let i=0;i<movie.length;i++)
+    {
+        if(movie[i].id==value){
+            found= true
+            res.send(movie[i])
+            break
+        }
+    }
+    if(found == false)
+    {
+        res.send("No movie exits with this id ")
+    }
+
 })
 
 
 
-
 module.exports = router;
+// adding this comment for no reason../
