f820b0a4cfe28e22873a2ad02e1b1958f940ee66
Mohdjafar786 <mj029044@gmail.com>
2022-03-05 01:35:41 +0530

    latest update

---
+++ src/controllers/controller.js
@@ -0,0 +1,16 @@
+const jafar=async function(req,res){
+    res.send({msg:"Hi,this is jafar "})
+}
+
+const mahir=async function(req,res){
+res.send({msg:"Hi,this is mahir"})
+}
+
+
+
+
+
+
+
+module.exports.jafar= jafar
+module.exports.mahir= mahir
--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
 const express = require('express');
+
 const bodyParser = require('body-parser');
+
 const route = require('./routes/route.js');
+const moment = require('moment')
+
 const { default: mongoose } = require('mongoose');
+
 const app = express();
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+app.use(function (req, res, next) {
+
+    //const dateTime = new Date().toISOString().replace(/\..+/, "");
+
+    let now = new Date();
+
+    const dateTime =  new moment(now).format('YYYY-MM-DD HH:mm:ss:a');
+
+    const IP = req.ip;
+
+    const Api = req.method + req.originalUrl;
+
+    console.log("Timestamp:", dateTime," "," IP address ", IP," ","API", Api);
+
+    
+    next();
+
+  });
+
 mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
+
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
