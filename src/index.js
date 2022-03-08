bcec2ae5883993bf36a6887f710b67a38d568c53
Mohdjafar786 <mj029044@gmail.com>
2022-03-09 01:57:16 +0530

    latest update

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Aafrin77:omaJBV2vPYhwOS7f@cluster0.ekfff.mongodb.net/MohdJafar7860", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
