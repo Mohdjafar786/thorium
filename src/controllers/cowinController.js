ef19f1eb8a22671af5c49d8b8a7338723eceff84
Mohdjafar786 <mj029044@gmail.com>
2022-03-12 13:46:03 +0530

    latest update

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -1,6 +1,5 @@
 let axios = require("axios")
 
-
 let getStates = async function (req, res) {
 
     try {
@@ -20,7 +19,7 @@ let getStates = async function (req, res) {
 }
 
 
-let getDistricts = async function (req, res) {
+let getByState = async function (req, res) {
     try {
         let id = req.params.stateId
         let options = {
@@ -57,29 +56,54 @@ let getByPin = async function (req, res) {
     }
 }
 
-let getOtp = async function (req, res) {
-    try {
-        let blahhh = req.body
-        
-        console.log(`body is : ${blahhh} `)
-        var options = {
-            method: "post",
-            url: `https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP`,
-            data: blahhh
-        }
 
-        let result = await axios(options)
-        console.log(result.data)
-        res.status(200).send({ msg: result.data })
+
+
+
+
+
+let getOtp = async function(req, res){
+try{
+    let inputData = req.body
+    if(inputData){
+    let options = {
+        method : "POST",
+        url : "https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP",
+        data : inputData
     }
-    catch (err) {
-        console.log(err)
-        res.status(500).send({ msg: err.message })
-    }
+    let result = await axios(options)
+    res.status(200).send({status: true, msg: result.data})
+}else{
+    res.status(400).send({status: false, msg:"please provide input data"})
+}
+}catch (err) {
+    res.status(500).send({error: err.message})
+}
 }
 
+let sessionsByDistrict = async function (req, res){
+    try{
+        let dis = req.query.district_id
+        let date = req.query.date
+      if(dis && date){
 
+        let options = {
+            method: "GET",
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${dis}&date=${date}`
+        }
+        let result = await axios(options)
+        res.status(200).send({status: true, msg: result.data})
+
+      }else{
+          res.status(400).send({status: false, msg: "please provide the input"})
+      }  
+
+    }catch (error){
+        res.status(500).send({error: error.message})
+    }
+}
 module.exports.getStates = getStates
-module.exports.getDistricts = getDistricts
+module.exports.getByState = getByState
 module.exports.getByPin = getByPin
 module.exports.getOtp = getOtp
+module.exports.sessionsByDistrict = sessionsByDistrict
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const CowinController= require("../controllers/cowinController")
+const CowinController = require('../controllers/cowinController')
+const WeatherController = require('../controllers/weatherController')
+const MemeController = require('../controllers/memeController')
 
 
 
@@ -8,15 +10,22 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-
 router.get("/cowin/states", CowinController.getStates)
-router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
+router.get("/cowin/getByState/:stateId", CowinController.getByState)
 router.get("/cowin/getByPin", CowinController.getByPin)
 
-router.post("/cowin/getOtp", CowinController.getOtp)
 
-// WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
+// GETOTP and vaccine sessions by district APIs
+router.post('/getOtp', CowinController.getOtp )
+router.get('/sessionsByDistrict', CowinController.sessionsByDistrict)
 
+// getWeather, tempOfLondon, tempOfCities
+router.get('/getWeather', WeatherController.getWeather)
+router.get('/tempOfLondon', WeatherController.tempOfLondon)
+router.get('/tempOfCities', WeatherController.tempOfCities)
 
+// 
+router.get('/getMemes', MemeController.getMemes )
+router.post('/createMemes', MemeController.createMemes)
 
 module.exports = router;
